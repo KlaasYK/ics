d60b1dedbea8c7ca289f9f058e231f81ccdcc5eb
Klaas Kliffen <klaasyk@users.noreply.github.com>
2015-12-14 11:07:24 +0100

    Model added

--- App/main.cpp
+++ App/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
+    // TODO: read out screen width and height
+    w.setGeometry(1920/2-400,1080/2-300,800,600);
     w.show();
 
     return a.exec();
