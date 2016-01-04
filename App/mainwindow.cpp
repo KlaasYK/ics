b5d2fcd14211f380fc0a539d03d5e23b2e73abcc
Jan Kramer <jankramer@gmail.com>
2016-01-04 21:33:28 +0100

    implemented Timer for start/stop button

--- App/mainwindow.cpp
+++ App/mainwindow.cpp
@@ -3,28 +3,41 @@
 
 #include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    sim = new Simulation();
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  sim = new Simulation();
+  timer = new QTimer(this);
+  connect(timer, SIGNAL(timeout()), this, SLOT(step()));
+  started = false;
 }
 
-MainWindow::~MainWindow()
-{
-    delete sim;
-    delete ui;
+MainWindow::~MainWindow() {
+  delete sim;
+  delete ui;
+  delete timer;
 }
 
-void MainWindow::on_StartStopBtn_clicked()
-{
-    qDebug() << "Start Stop clicked";
-}
-void MainWindow::on_StepBtn_clicked()
-{
-    qDebug() << "Step clicked";
-    sim->doSimulationStep();
+void MainWindow::step() {
+  qDebug() << "performing a step";
+  sim->doSimulationStep();
+  ui->trafficview->setState(sim->getIntersections(), sim->getCars());
 }
 
-
+void MainWindow::on_StartStopBtn_clicked() {
+  qDebug() << "Start Stop clicked";
+  if (started) {
+    timer->stop();
+    started = false;
+  } else {
+    // every 500 ms
+    timer->start(500);
+    started = true;
+  }
+}
+void MainWindow::on_StepBtn_clicked() {
+  qDebug() << "Step clicked";
+  if (!started) {
+    step();
+  }
+}
--- App/mainwindow.h
+++ App/mainwindow.h
@@ -2,27 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+
 #include "simulation.h"
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
 
 private slots:
-    void on_StepBtn_clicked();
-    void on_StartStopBtn_clicked();
+  void step();
+  void on_StepBtn_clicked();
+  void on_StartStopBtn_clicked();
 
 private:
-    Ui::MainWindow *ui;
-    Simulation *sim;
+  Ui::MainWindow *ui;
+  Simulation *sim;
+  QTimer *timer;
+  bool started;
 };
 
 #endif // MAINWINDOW_H
